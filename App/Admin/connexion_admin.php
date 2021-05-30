5af2b45491df21bf191da9c02c0f1f2e3e73ac50
mouadhakimi <75374084+mouadhakimi@users.noreply.github.com>
2021-05-31 00:57:09 +0100

    Update connexion_admin.php

--- App/Admin/connexion_admin.php
+++ App/Admin/connexion_admin.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="../css/connexion_admin.css">
+        <link rel="stylesheet" href="../css/connexion.css">
     </head>
     <body>
             <div class="page-header">
