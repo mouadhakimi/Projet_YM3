dea9fc6e1a0832a5d8027b5dbcbe164b31229a71
mouadhakimi <75374084+mouadhakimi@users.noreply.github.com>
2021-05-31 00:48:24 +0100

    Update change_password.php

--- App/Admin/change_password.php
+++ App/Admin/change_password.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Changer de mot de passe</title>
-        <link rel="icon" type="image/png" sizes="16x16" href="../../Photo/yncréamaroc.jpg">
+        <link rel="icon" type="image/png" sizes="16x16" href="../../Photo/yncréa_maroc.png">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="../css/avis.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
