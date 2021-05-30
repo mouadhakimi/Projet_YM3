dcc2cc5d4638e6834a096e49548b74cebf3cd4c1
mouadhakimi <75374084+mouadhakimi@users.noreply.github.com>
2021-05-31 00:50:04 +0100

    Update ISA.php

--- App/Choix_des_filieres/ISA.php
+++ App/Choix_des_filieres/ISA.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<link rel="icon" type="image/png" sizes="16x16" href="../../Photo/yncréamaroc.jpg">
+<link rel="icon" type="image/png" sizes="16x16" href="../../Photo/yncréa_maroc.png">
 
 <title>ISA Lille</title>
 
