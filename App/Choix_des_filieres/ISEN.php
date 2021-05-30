688d23282454cd3f2fb64714dbe7a0cdcb1a2e09
mouadhakimi <75374084+mouadhakimi@users.noreply.github.com>
2021-05-31 00:50:09 +0100

    Update ISEN.php

--- App/Choix_des_filieres/ISEN.php
+++ App/Choix_des_filieres/ISEN.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<link rel="icon" type="image/png" sizes="16x16" href="../../Photo/yncréamaroc.jpg">
+<link rel="icon" type="image/png" sizes="16x16" href="../../Photo/yncréa_maroc.png">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <title>ISEN Lille</title>
