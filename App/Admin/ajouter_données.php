2e4214536b7e7ebcd7325f48415b5ce10550f844
mouadhakimi <75374084+mouadhakimi@users.noreply.github.com>
2021-06-01 10:06:55 +0100

    Update ajouter_données.php

--- App/Admin/ajouter_données.php
+++ App/Admin/ajouter_données.php
@@ -8,16 +8,15 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
 die("Échec de la connexion : " . mysqli_connect_error()."<br>");
 }
-$nom = $_POST['nom'];
-$ecole = $_POST['ecole'];
+$nom = $_POST['filiere'];
+$colonne = $_POST['columns'];
 $definition = $_POST['definition'];
-$salaire = $_POST['Salaire'];
-$formation = $_POST['formation'];
-$employeurs = $_POST['principaux_employeurs'];
-$qualités = $_POST['qualités_compétences_nécessaires'];
-$lien = $_POST['lien'];
-if($conn->query("INSERT INTO `filiere` (`filiere_id`, `nom_filiere`, `ecole`, `definition`, `salaire`, `formation`, `principaux_employeurs`, `qualités_compétences_nécéssaires`, `lien`) VALUES (NULL, '$nom', '$ecole', '$definition', '$salaire', '$formation', '$employeurs', '$qualités', '$lien') ")==TRUE or die($conn->error)){
-    echo "<script>window.location.replace('http://127.0.0.1/Projet/App/Admin/traitement_filiere.php');</script>";
-    echo "<script>alert('$nom_filiere,est ajouter avec succés');</script>";
+$donnée = $_POST['donnée'];
+$requette_2 = "UPDATE filiere SET $colonne ='$donnée' WHERE nom_filiere = '$nom'";
+if ($conn->query($requette_2) === TRUE) {
+    echo "<script>alert('Donnée modifier');</script>";
+    echo "<script>window.location.replace('http://127.0.0.1/Projet/App/Admin/ajout_données.php');</script>";
+} else {
+    echo "Error updating record: " . $conn->error;
 }
 ?>
