f9103ad3e36b1862e75ab1403792f542646628c0
mouadhakimi <75374084+mouadhakimi@users.noreply.github.com>
2021-06-02 15:09:57 +0100

    Update Avis.php

--- App/Avis/Avis.php
+++ App/Avis/Avis.php
@@ -2,7 +2,7 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8" />
-        <title>Ajouter un administrateur</title>
+        <title>Avis</title>
         <link rel="icon" type="image/png" sizes="16x16" href="../../Photo/yncréa_maroc.png">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="../css/avis.css">
