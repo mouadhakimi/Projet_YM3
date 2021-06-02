f2b7ce1679a12093bd5688e83e3ed54e7760b453
mouadhakimi <75374084+mouadhakimi@users.noreply.github.com>
2021-06-02 15:16:58 +0100

    Update maps.js

--- App/js/maps.js
+++ App/js/maps.js
@@ -1,14 +1,14 @@
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat:33.99728965143808, lng: -6.844357952656807};
-    // The map, centered at Uluru
+    // The location of Yncréa Maroc
+    const yncrea = { lat:33.99728965143808, lng: -6.844357952656807};
+    // The map, centered at Yncréa Maroc
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
-      center: uluru,
+      center: yncrea,
     });
-    // The marker, positioned at Uluru
+    // The marker, positioned at Yncréa Maroc
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: yncrea,
       map: map,
     });
   }
