bd2189d36d932ff02d809017893704d480169667
mouadhakimi <75374084+mouadhakimi@users.noreply.github.com>
2021-06-02 13:22:11 +0100

    Create etudiant_concours.php

---
+++ App/Admin/etudiant_concours.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<script>alert('Les étudiants enregistrés sont :');</script>";
+    echo "<script>window.location.replace('https://docs.google.com/spreadsheets/d/1J9b5XCMlfL-EqrTp8Wg8MWofI7dtDt2S_0u3Xkiz9qY/edit?usp=sharing');</script>";
+?>
