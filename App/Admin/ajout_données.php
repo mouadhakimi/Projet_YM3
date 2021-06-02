fdcc1ec1175a76a31d880b34ea7a0a6cc7ba2c19
mouadhakimi <75374084+mouadhakimi@users.noreply.github.com>
2021-06-02 14:07:27 +0100

    Update ajout_données.php

--- App/Admin/ajout_données.php
+++ App/Admin/ajout_données.php
@@ -10,6 +10,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <script src="../js/dropdown.js"></script>
+        <link rel="stylesheet" type="text/css" href="../../App/css/maps.css" />
+        <script src="../js/maps.js"></script>
         <?php
         $servername = "127.0.0.1";
         $database = "projet_ym3";
@@ -78,5 +80,65 @@
                 </form>
             </div>
         </div>
+        <br>
+        <br>
+        <footer class="page-footer font-small stylish-color-dark pt-4" style="background-color: rgb(221, 202, 202);">
+          <div class="container text-center text-md-left">
+            <div class="row">
+              <div class="col-md-4 mx-auto">
+                <p><img
+                  src="../../Photo/yncréamaroc.png"
+                  alt="Logo yncréa Maroc"></p>
+              </div>
+              <hr class="clearfix w-100 d-md-none">
+              <div class="col-md-2 mx-auto">
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Contacts :</h5>
+                <ul class="list-unstyled">
+                  <li>
+                    <a class="btn btn-primary" style="background-color: #27dd73;width: 241px;" href="https://api.whatsapp.com/send?phone=212661754018" role="button" target="_blank"><img id="whastaap" src="../../Photo/whatsapp.png" style=" width: 25px;"> Responsable d'admission</a>
+                  </li>
+                  <br>
+                  <li>
+                    <a class="btn btn-primary" style="background-color: #3db2dd;width: 241px;" href="mailto:yousra.el-hassani@yncrea.fr?subject=Demande de renseignements" role="button" target="_blank"><img id="whastaap" src="../../Photo/mail.png" style=" width: 25px;"> Responsable d'admission</a>
+                  </li>
+                  <br>
+                  <li>
+                    <a class="btn btn-primary" style="background-color: #3db2dd;width: 233px;" href="mailto:jean-marc.fontugne@yncrea.fr?subject=Demande de renseignements" role="button" target="_blank"><img id="whastaap" src="../../Photo/mail.png" style=" width: 25px;"> Directeur des opérations</a>
+                  </li>
+                </ul>
+              </div>
+              <hr class="clearfix w-100 d-md-none">
+              <div class="col-md-2 mx-auto">
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+                <nav class="nav flex-column">
+                  <a class="nav-link" href="../Choix_des_filieres/choix_ecole.php">Filiéres</a>
+                  <a class="nav-link" href="../main/admission.php">Admission</a>
+                  <a class="nav-link disabled">Administrateur</a>
+                  <a class="nav-link" href="../main/admission.php">Frais de scolarité</a>
+                </nav>
+              </div>
+              <hr class="clearfix w-100 d-md-none">
+              <div id="map"></div>
+              <script
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCG6HKPiiC2B1W_mYlYx5MmtK9Vk09cQ4w&callback=initMap&libraries=&v=weekly"
+                async
+              ></script>
+            </div>
+          </div>
+          <hr>
+          <ul class="list-unstyled list-inline text-center py-2">
+            <li class="list-inline-item">
+              <h5 class="mb-1">Donnez votre avis !</h5>
+            </li>
+            <li class="list-inline-item">
+              <a href="../Avis/Avis.php" class="btn btn-danger btn-rounded">Avis</a>
+            </li>
+          </ul>
+          <hr>
+          <div class="footer-copyright text-center py-3">© 2021 Copyright:
+            <a href="https://yncreamaroc.ma/"> Yncréa Maroc</a>
+            <p>Tous droits réservés</p>
+          </div>
+        </footer>
     </body>
 </html>
